a4252281e9382619866276471add3a4392cb5d06
NicolasH67 <hecker.nicolas.pro@gmail.com>
2021-07-15 17:21:09 +0200

    backend user file

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+require('dotenv').config;
+
+const app = express(); 
+
+app.use(helmet()); 
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+})
