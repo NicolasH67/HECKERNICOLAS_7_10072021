66613380c03eb2437d6ccd0fe530e7b94b59eceb
NicolasH67 <hecker.nicolas.pro@gmail.com>
2021-07-19 11:49:13 +0200

    add signup function

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
-// function signup
-exports.signup =(req, res, next) => {
+// signup function
+exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
@@ -18,4 +18,29 @@ exports.signup =(req, res, next) => {
                 .catch(error => res.status(400).json({ error }))
         })
         .catch(error => res.status(500).json({ error }))
+}
+
+// login function
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'User not found'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.stauts(401).json({ error: 'Incorrect Password!' }); 
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }))
+        })
 }
